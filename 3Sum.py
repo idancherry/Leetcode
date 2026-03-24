5b8691f670e190f7ed52891cf6357ed99ae7a75a
idancherry <idancherry@gmail.com>
2026-03-24 08:23:21 +0200

    15. 3Sum

---
+++ 3Sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res=[]
+        for i in range(len(nums)):
+            if nums[i]>0:
+                break
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            L=i+1
+            R=len(nums)-1
+            while L<R:
+                summ = nums[i]+nums[L]+nums[R]
+                if summ==0:
+                    res.append([nums[i], nums[L], nums[R]])
+                    L+=1
+                    R-=1
+                    while L<R and nums[L]==nums[L-1]:
+                        L+=1
+                    while L<R and nums[R]==nums[R+1]:
+                        R-=1
+                elif summ>0:
+                    R-=1
+                else:
+                    L+=1
+
+        return res
